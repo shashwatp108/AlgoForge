0c06e45ff95fee736cc081f47d6e4bfb625b0c99
Shashwat <shashwatp108@gmail.com>
2025-12-04 22:07:43 +0530

    fixed deployment issues, due to link mismatch.

--- server/generateFile.js
+++ server/generateFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuid } = require('uuid');
 
-const dirCodes = path.join(__dirname, 'runner_workspace', 'codes'); 
+const dirCodes = path.join(__dirname, 'codes');
 
 if (!fs.existsSync(dirCodes)) {
     fs.mkdirSync(dirCodes, { recursive: true });
